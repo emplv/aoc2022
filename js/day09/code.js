dd81922ae3b953edcebfc40651a23e6aab7922e5
emplv <emils@plavenieks.lv>
2022-12-09 12:52:04 +0200

    refactor day 9

--- js/day09/code.js
+++ js/day09/code.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const { test, solve } = require("../utils");
 
 // Path valid if running from "js/day09"
 
@@ -7,124 +8,53 @@ const testFileContent = fs.readFileSync(path.resolve("./test.txt"), "utf-8");
 const test2FileContent = fs.readFileSync(path.resolve("./test2.txt"), "utf-8");
 const fileContent = fs.readFileSync(path.resolve("./input.txt"), "utf-8");
 
-function p1(input) {
-  const positions = new Set(["0-0"]);
-  const HT = [
-    [0, 0],
-    [0, 0],
-  ];
+function getResult(input, ropeSize = 2) {
+  const positions = new Set();
+  let rope = [...Array(ropeSize)].map((_) => [0, 0]);
   input.split("\n").map((instruction) => {
     const [direction, count] = instruction.split(" ");
     for (let i = 0; i < count; i++) {
-      let [ly, lx] = HT[0];
-      let [[hy, hx], [ty, tx]] = HT;
-      HT[0] = [
-        (hy = hy + Number(direction === "U") - Number(direction === "D")),
-        (hx = hx + Number(direction === "R") - Number(direction === "L")),
-      ];
-      if (Math.abs(hy - ty) > 1 || Math.abs(hx - tx) > 1) {
-        HT[1] = [ly, lx];
-        positions.add(`${ly}-${lx}`);
-      }
+      // move rope
+      rope.map((pos, i, rope) => {
+        const [y, x] = pos;
+        if (i === 0) {
+          // move head
+          pos = [
+            y + Number(direction === "U") - Number(direction === "D"),
+            x + Number(direction === "R") - Number(direction === "L"),
+          ];
+        } else {
+          // move rest
+          const [pY, pX] = rope[i - 1];
+          if (Math.abs(pY - y) > 1 || Math.abs(pX - x) > 1) {
+            pos = [y + Math.sign(pY - y), x + Math.sign(pX - x)];
+          }
+          // log tail position
+          if (i === rope.length - 1) {
+            positions.add(`${pos[0]} ${pos[1]}`);
+          }
+        }
+        return (rope[i] = pos);
+      });
     }
   });
   return positions.size;
 }
 
-console.log(
-  `Test input is ${
-    p1(testFileContent) === 13 ? "\x1b[42m" : "\x1b[41min"
-  }correct\x1b[0m!`
-);
-console.log("Results is: ", p1(fileContent));
+function p1(input) {
+  return getResult(input, 2);
+}
+
+test(p1(testFileContent), 13);
+solve(p1(fileContent));
 
 // /**
 //  * PART 2
 //  */
 
 function p2(input) {
-  const positions = new Set();
-  let rope = [...Array(10)].map((_) => [0, 0]);
-  input.split("\n").map((instruction) => {
-    const [direction, count] = instruction.split(" ");
-    for (let i = 0; i < +count; i++) {
-      // move rope
-      rope.map((pos, i, rope) => {
-        const [y, x] = pos;
-        if (i === 0) {
-          // move head
-          pos = [
-            y + Number(direction === "U") - Number(direction === "D"),
-            x + Number(direction === "R") - Number(direction === "L"),
-          ];
-        } else {
-          // move rest
-          const [pY, pX] = rope[i - 1];
-          if (Math.abs(pY - y) > 1 || Math.abs(pX - x) > 1) {
-            pos = [y + Math.sign(pY - y), x + Math.sign(pX - x)];
-          }
-          // log tails positions
-          if (i === rope.length - 1) {
-            positions.add(`${pos[0]} ${pos[1]}`);
-          }
-        }
-        return (rope[i] = pos);
-      });
-    }
-  });
-  return positions.size;
+  return getResult(input, 10);
 }
 
-console.log(
-  `Test input is ${
-    p2(test2FileContent) === 36 ? "\x1b[42m" : "\x1b[41min"
-  }correct\x1b[0m!`
-);
-console.log("Results is: ", p2(fileContent));
-
-
-// Extra refactor
-function gerResult(input, ropeSize) {
-  const positions = new Set();
-  let rope = [...Array(ropeSize)].map((_) => [0, 0]);
-  input.split("\n").map((instruction) => {
-    const [direction, count] = instruction.split(" ");
-    for (let i = 0; i < +count; i++) {
-      // move rope
-      rope.map((pos, i, rope) => {
-        const [y, x] = pos;
-        if (i === 0) {
-          // move head
-          pos = [
-            y + Number(direction === "U") - Number(direction === "D"),
-            x + Number(direction === "R") - Number(direction === "L"),
-          ];
-        } else {
-          // move rest
-          const [pY, pX] = rope[i - 1];
-          if (Math.abs(pY - y) > 1 || Math.abs(pX - x) > 1) {
-            pos = [y + Math.sign(pY - y), x + Math.sign(pX - x)];
-          }
-          // log tails positions
-          if (i === rope.length - 1) {
-            positions.add(`${pos[0]} ${pos[1]}`);
-          }
-        }
-        return (rope[i] = pos);
-      });
-    }
-  });
-  return positions.size;
-}
-console.log(
-  `Test input is ${
-    gerResult(testFileContent, 2) === 13 ? "\x1b[42m" : "\x1b[41min"
-  }correct\x1b[0m!`
-);
-console.log("Results is: ", gerResult(fileContent, 2));
-console.log(
-  `Test input is ${
-    gerResult(test2FileContent, 10) === 36 ? "\x1b[42m" : "\x1b[41min"
-  }correct\x1b[0m!`
-);
-console.log("Results is: ", gerResult(fileContent, 10));
+test(p2(test2FileContent), 36);
+solve(p2(fileContent));
--- js/utils.js
+++ js/utils.js
@@ -1,22 +1,35 @@
-const sum = arr => arr.reduce((sum, item) => sum + +item, 0);
-const sumLines = string => eval(string.replace(/\n/g,'+'));
-const sort = (arr, asc = false) => [...arr].sort((a,b)=> asc ? a-b : b-a);
-const max = (arr, count = 1) => count === 1 ? Math.max(...arr) : sort(arr).slice(0, count);
-const min = (arr, count = 1) => count === 1 ? Math.min(...arr) : sort(arr, true).slice(0, count);
+const sum = (arr) => arr.reduce((sum, item) => sum + +item, 0);
+const sumLines = (string) => eval(string.replace(/\n/g, "+"));
+const sort = (arr, asc = false) =>
+  [...arr].sort((a, b) => (asc ? a - b : b - a));
+const max = (arr, count = 1) =>
+  count === 1 ? Math.max(...arr) : sort(arr).slice(0, count);
+const min = (arr, count = 1) =>
+  count === 1 ? Math.min(...arr) : sort(arr, true).slice(0, count);
 const chunk = (arr, chunkSize = 1) => {
-    const res = [];
-    for (let i = 0; i < arr.length; i += chunkSize) {
-        const chunk = arr.slice(i, i + chunkSize);
-        res.push(chunk);
-    }
-    return res;
-}
+  const res = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    res.push(chunk);
+  }
+  return res;
+};
+const test = (result, correct) =>
+  console.log(
+    `Test input is ${
+      result === correct ? "\x1b[42m" : "\x1b[41min"
+    }correct\x1b[0m!`
+  );
 
-module.exports ={
-    sum,
-    sumLines,
-    max,
-    min,
-    sort,
-    chunk,
+const solve = (result) => console.log("Results is: ", result);
+
+module.exports = {
+  sum,
+  sumLines,
+  max,
+  min,
+  sort,
+  chunk,
+  test,
+  solve,
 };
